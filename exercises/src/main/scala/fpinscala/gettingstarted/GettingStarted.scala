453e8a765c34e8940c285f0ff633cbafbcd6a263
Ruben S <ruben@txodds.com>
2018-03-20 15:51:49 +0100

    2.5

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -191,8 +191,7 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 
   def main(args: Array[String]): Unit = {
     println(isSorted(Array(1,2,3),greaterThan)+" == true")
